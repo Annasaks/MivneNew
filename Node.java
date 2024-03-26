95e1c2778b380258a382cb3bb0d9123863ef9e75
annasaks <anna.sakoun@campus.technion.ac.il>
2024-03-26 14:45:42 +0200

    add node

--- Node.java
+++ Node.java
@@ -1,13 +1,16 @@
-class Node {
+public class Node {
     Node left ;
     Node right;
     Node middle ;
     Node parent;
     int key ;
     int size ;
+    Runner runner;
 
-    public TreeNode(int degree, boolean leaf) {
-
-    }
+    public Node(int key, Runner runner) {
+            this.key = key;
+            this.size = 1;
+            this.runner = runner;
+        }
 }
 
